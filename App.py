64c70701ffd0806467c9e04f8da4c17b519384c5
shawn3298317 <shawn3298317lin@gmail.com>
2019-11-03 14:29:14 -0500

    Finish UI for login page; Restructured static/

--- App.py
+++ App.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://shawn:18740811@h4iminihack-73gfh.mongodb.net/test?retryWrites=true&w=majority"
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/socialMedia"
 
+@app.route("/login", methods=["GET", "POST", "PUT"])
+def login():
+	"""Serve login page template."""
+	return render_template("login.html")
+
 mongo = PyMongo(app)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
