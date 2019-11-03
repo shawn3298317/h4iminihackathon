8d5ac71b027c8b20fb658c75f6d8c7a729b5a290
shawn3298317 <shawn3298317lin@gmail.com>
2019-11-03 13:58:55 -0500

    Update gitignore & update main page

--- App.py
+++ App.py
@@ -9,9 +9,11 @@ from flask_pymongo import PyMongo
 
 # initializes app
 
-# app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/socialMedia"
-# mongo = PyMongo(app)
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=4000, debug=True)
-#
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://shawn:18740811@h4iminihack-73gfh.mongodb.net/test?retryWrites=true&w=majority"
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/socialMedia"
+
+mongo = PyMongo(app)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
+
