b4ee2b5a91944925fa0332376848ce83e58b136a
shawn3298317 <shawn3298317lin@gmail.com>
2019-11-03 16:20:19 -0500

    Compete login flow

--- App.py
+++ App.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, url_for, request, redirect
 from flask_pymongo import PyMongo
+import json
 
 from pymongo import MongoClient
 
@@ -18,6 +19,14 @@ def add_new_user_to_db(name, email, pw):
     db = client.happy
     users = db.users
     users.insert_one({"name": name, "email": email, "password": pw})
+
+def find_user_from_db(email, pw):
+    client = MongoClient(app.config["MONGO_URI"])
+    db = client.happy
+    users = db.users
+    user = users.find_one({"email": email, "password": pw})
+    print(user)
+    return user
 
 
 @app.route('/')
@@ -50,11 +59,13 @@ def login():
 
     if request.method == "POST":
         print(form, form.get("email"), form.get("password"))
+        msg = find_user_from_db(form.get("email"), form.get("password"))
         # TODO: verify with database
-        if form.get("email") != "admin@bu.edu" or form.get("password") != "admin":
-            error = "Invalid Credentials. Please try again."
+        # if form.get("email") != "admin@bu.edu" or form.get("password") != "admin":
+        if msg is None:
+            error = "Cannot find this user. Please try again."
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('home', messages=msg))
     return render_template("login.html", error=error, form=form)
 
 @app.route("/signup", methods=["GET", "POST", "PUT"])
